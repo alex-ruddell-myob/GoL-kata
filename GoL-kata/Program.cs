d453be24dc9cf3efd4a8e051ea9dbc4c116b8c57
alex-ruddell-myob <alex.ruddell@myob.com>
2020-12-01 08:22:04 +1300

    First commit

---
+++ GoL-kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoL_kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
