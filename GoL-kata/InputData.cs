e24915ad37cfec5697dc586203dade76893a926c
alex-ruddell-myob <alex.ruddell@myob.com>
2020-12-02 12:28:49 +1300

    Game implemented

--- GoL-kata/InputData.cs
+++ GoL-kata/InputData.cs
@@ -8,28 +8,5 @@ namespace GoL_kata
         public int seedWidth;
 
         public string[] seedString;
-        
-        // TODO: Decide if I actually want this at all. If yes, move.
-        /*public bool[,] seedArray;
-
-        public void ConvertStringToSeed()
-        {
-            // Do this in the ConsoleIO application?
-            // TODO: Unit test this!
-            for (int i = 0; i < seedHeight; i++)
-            {
-                for (int j = 0; j < seedWidth; j++)
-                {
-                    if (seedString[i][j] == 'X')
-                    {
-                        seedArray[i, j] = true;
-                    }
-                    else
-                    {
-                        seedArray[i, j] = false;
-                    }
-                }
-            }
-        }*/
     }
 }
--- GoL-kata/Program.cs
+++ GoL-kata/Program.cs
@@ -10,9 +10,9 @@ namespace GoL_kata
             Initialise();
             
             InputData userInput = ConsoleIO.ReadUserInput();
-            //userInput.ConvertStringToSeed(); // FIX THIS?
 
             var board = new Board(userInput);
+            
             int iteration = 0;
             ConsoleIO.PrintGameBoard(board, iteration);
             
@@ -20,24 +20,38 @@ namespace GoL_kata
             {
                 board = Game.Iterate(board);
                 iteration++;
+                
                 ConsoleIO.PrintGameBoard(board, iteration);
 
                 Console.Write("\nPress ANY KEY to end game...");
-                Thread.Sleep(2000);
+                Thread.Sleep(1000);
             }
-
-            // USER INPUT creates BOARD object DONE!!
-            // BOARD object instantiates cells DONE!!
-            // BOARD is passed through GAME to alter state until the game finishes
-            // BOARD is printed each time.
-
-            
-
         }
 
         static void Initialise()
         {
             Console.WriteLine("Welcome to Conway's Game of Life!\n");
+            
+            // PULSAR SEED
+            InputData pulsarInput = new InputData();
+            pulsarInput.boardHeight = 17;
+            pulsarInput.boardWidth = 17;
+            pulsarInput.seedHeight = 13;
+            pulsarInput.seedWidth = 13;
+            pulsarInput.seedString = new string[13];
+            pulsarInput.seedString[0] = "--XXX---XXX--";
+            pulsarInput.seedString[1] = "-------------";
+            pulsarInput.seedString[2] = "X----X-X----X";
+            pulsarInput.seedString[3] = "X----X-X----X";
+            pulsarInput.seedString[4] = "X----X-X----X";
+            pulsarInput.seedString[5] = "--XXX---XXX--";
+            pulsarInput.seedString[6] = "-------------";
+            pulsarInput.seedString[7] = "--XXX---XXX--";
+            pulsarInput.seedString[8] = "X----X-X----X";
+            pulsarInput.seedString[9] = "X----X-X----X";
+            pulsarInput.seedString[10] = "X----X-X----X";
+            pulsarInput.seedString[11] = "-------------";
+            pulsarInput.seedString[12] = "--XXX---XXX--";
         }
     }
 }
